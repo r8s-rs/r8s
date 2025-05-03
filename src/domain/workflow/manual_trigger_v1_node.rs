d3433f68292dd0cb5a55f357a77f039763779267
Cosmo André <soucosmomoraes@gmail.com>
2025-05-03 19:10:14 -0300

    adicionado retorno de tipo string do nó

--- src/domain/workflow/manual_trigger_v1_node.rs
+++ src/domain/workflow/manual_trigger_v1_node.rs
@@ -1,4 +1,11 @@
-use serde::Deserialize;
+use crate::domain::entities::NodeBase;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
-pub struct ManualTriggerV1Node {}
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct ManualTriggerV1Node;
+
+impl NodeBase for ManualTriggerV1Node {
+    fn get_type(&self) -> &'static str {
+        "SetV1"
+    }
+}
