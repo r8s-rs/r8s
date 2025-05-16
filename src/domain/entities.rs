20cb0d36232d5ca7b02654918eaf85fd837c4418
Cosmo André <soucosmomoraes@gmail.com>
2025-05-15 23:28:27 -0300

    Adicionado http method

--- src/domain/entities.rs
+++ src/domain/entities.rs
@@ -1,12 +1,14 @@
 pub mod partitions;
 mod edge_condition;
 mod http_request;
+mod http_method;
 mod node_kind;
 mod node_base;
 mod node;
 
 pub use edge_condition::EdgeCondition;
 pub use http_request::HttpRequest;
+pub use http_method::HttpMethod;
 pub use node_kind::NodeKind;
 pub use node_base::NodeBase;
 pub use node::Node;
