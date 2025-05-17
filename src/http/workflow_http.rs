e49675995cd2ee2da238a76973c334761e1b0403
Cosmo André <soucosmomoraes@gmail.com>
2025-05-17 14:16:53 -0300

    Adicionado bloqueio criação de fluxo sem nós conectados

--- src/http/workflow_http.rs
+++ src/http/workflow_http.rs
@@ -1,5 +1,5 @@
 use crate::infrastructure::repositories::{WorkflowRepository, Workflow as WorkflowEntity};
-use tracing::info;
+use tracing::{info, trace};
 use crate::State;
 use actix_web::{
     HttpResponse,
@@ -15,6 +15,34 @@ pub struct WorkflowHttp;
 impl WorkflowHttp {
     pub async fn store(wf: Json<WorkflowEntity>, state: Data<State>) -> impl Responder {
         let wf = wf.into_inner();
+
+        let (mut has_trigger, mut has_node) = (false, false);
+
+        for node in wf.nodes.values() {
+            if has_trigger && has_node {
+                break;
+            }
+
+            let kind = node.get_kind();
+
+            if !has_trigger && kind.is_trigger() {
+                has_trigger = node
+                    .next
+                    .as_ref()
+                    .map_or(false, |next| next.iter().any(|key| wf.nodes.contains_key(key)));
+            }
+
+            if !has_node && !kind.is_trigger() {
+                has_node = true;
+            }
+        }
+
+        trace!(has_trigger, has_node);
+
+        if !has_trigger || !has_node {
+            return HttpResponse::BadRequest()
+                .body("The workflow must have at least one trigger and one node connected");
+        }
 
         info!("Saving workflow [{}]", wf.pub_id);
 
