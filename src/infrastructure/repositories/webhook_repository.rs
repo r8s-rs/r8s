70266ab5d213099c950b3a6e78f2d36f9266afdb
Cosmo André <soucosmomoraes@gmail.com>
2025-06-08 17:18:01 -0300

    Melhorias no repositorio de webhook

--- src/infrastructure/repositories/webhook_repository.rs
+++ src/infrastructure/repositories/webhook_repository.rs
@@ -1,12 +1,31 @@
 
 use crate::domain::entities::{HttpRequest, HttpMethod};
 use sqlx::{PgPool, Error, Transaction, Postgres};
+use crate::domain::workflow::WebhookV1Node;
+
 use serde_json::json;
 use super::Webhook;
 
 pub struct WebhookRepository;
 
 impl WebhookRepository {
+    pub async fn get_by_workflow_id(tx: &mut Transaction<'_, Postgres>, workflow_id: i64) -> Result<Option<WebhookV1Node>, Error> {
+        sqlx::query_as(
+            r#"
+            select
+                path,
+                method,
+                response_code
+            from
+                webhook
+                where
+                workflow_id = $1
+            "#,
+        )
+        .bind(workflow_id)
+        .fetch_optional(&mut **tx)
+        .await
+    }
     pub async fn get_by_path(pool: &PgPool, path: &str, method: &HttpMethod) -> Result<Option<Webhook>, Error> {
         sqlx::query_as(
             r#"
