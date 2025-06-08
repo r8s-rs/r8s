2b74d74e66a4a057d584b11a5232d141d7aacb03
Cosmo André <soucosmomoraes@gmail.com>
2025-06-08 16:01:44 -0300

    Removido repositories

--- src/infrastructure/repositories/execution.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Execution {
-    pub workflow_id: i64,
-    pub input: Value
-}
