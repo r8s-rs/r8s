bb342c3420ad80a329cb95613ee45dbccf82b502
Cosmo André <soucosmomoraes@gmail.com>
2025-06-23 23:49:11 -0300

    Adicionado template render

--- src/application.rs
+++ src/application.rs
@@ -1,7 +1,9 @@
+mod template_render;
 mod partitions;
 mod executor;
 mod state;
 
+pub use template_render::TemplateRender;
 pub use partitions::Partitions;
 pub use executor::Executor;
 pub use state::State;
